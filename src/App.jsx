4029eaf2b2993c0b32e348275573508731ce9436
SantiPelaez07 <santipelaez0@gmail.com>
2025-10-14 19:19:54 -0500

    Creación y configuración inicial del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Navbar } from './components/navBar/Navbar'
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <p>Estoy desde app</p>
+    </>
+  )
+}
+
+export default App
